eed2964b90235f92d04eac7f64a45d2f98dfe22c
Robert A Fraser <robert.fraser@uqconnect.edu.au>
2020-11-26 22:41:34 +1000

    Nicer round start message

--- gamemodes/sledbuild/gamemode/cl_init.lua
+++ gamemodes/sledbuild/gamemode/cl_init.lua
@@ -5,5 +5,7 @@ include("sh_convars.lua")
 
 -- Play sound on round start
 net.Receive("SBStartRound", function()
+    local number = net.ReadInt()
+    chat.AddText(color_white, "Race ", Color(0, 168, 255), number, color_white, " has begun!")
     surface.PlaySound("ambient/alarms/warningbell1.wav")
 end)
--- gamemodes/sledbuild/gamemode/sv_round.lua
+++ gamemodes/sledbuild/gamemode/sv_round.lua
@@ -1,14 +1,25 @@
+function GM:GetNextRaceNumber()
+    GetGlobalInt("NextRace", 1)
+end
+
+function GM:UpdateNextRaceNumber()
+    SetGlobalInt("NextRace", self:GetNextRaceNumber() + 1)
+end
+
 -- Start a new round
 function GM:StartRound()
     -- Network to clients
     net.Start("SBStartRound")
+        net.WriteInt(self:GetNextRaceNumber())
     net.Broadcast()
-
-    PrintMessage(3, "The race has begun!")
 
     -- Handle round pushing
     self:EnableRoundPush()
     timer.Simple(10, self.DisableRoundPush)
+end
+
+function GM:EndRound()
+
 end
 
 -- Enable the "pusher" at the start of a round
